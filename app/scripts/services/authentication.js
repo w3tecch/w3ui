bdd1a83ab5e34da6e0c81b75d4b72a6c94f00d33
Gery Hirschfeld <hirschi1988@gmail.com>
2014-01-13 20:53:27 +0100

    added login, auth and config

---
+++ app/scripts/services/authentication.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('w3uiFrontendApp')
+  .factory('Authentication', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
---
+++ app/scripts/services/configuration.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular.module('w3uiFrontendApp')
+  .constant('Configuration', 42);
