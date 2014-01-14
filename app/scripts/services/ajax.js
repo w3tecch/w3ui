71bed41486d4e585571b5e34c2fcc650f1cd33a2
Gery Hirschfeld <hirschi1988@gmail.com>
2014-01-14 16:27:11 +0100

    init ajax factory

---
+++ app/scripts/services/ajax.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('w3uiFrontendApp')
+
+/**
+ * AJAX Service
+ */
+    .factory('Ajax', function () {
+        // Service logic
+        var url = '';
+        var token = '';
+
+        
+
+
+        // Public API here
+        return {
+            setUrl: function( newUrl ){
+                url = newUrl;
+            },
+            setToken: function( newToken ){
+                token = newToken;
+            },
+            get: function () {
+
+            },
+            post: function () {
+
+            },
+            put: function () {
+
+            },
+            delete: function () {
+
+            }
+        };
+    });
