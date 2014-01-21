ba979a353528a80607b7912301d5b4c7bb5e1577
Gery Hirschfeld <hirschi1988@gmail.com>
2014-01-21 17:17:19 +0100

    updated login style

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,32 +13,29 @@ angular.module('w3uiFrontendApp', [
   */
 .config(function ($stateProvider, $urlRouterProvider) {
 
-    //$urlRouterProvider.otherwise("/rest");
-
     $urlRouterProvider.otherwise('/home');
 
-    $stateProvider
-        .state('master', {
-            access: 'admin',
-            url: '/',
-            views: {
-                'header': {
-                    controller: 'HeaderCtrl',
-                    templateUrl: 'views/master/header/header.view.html'
-                },
-                'main': {
-                    templateUrl: 'views/master/main.view.html'
-                },
-                'navigation@master': {
-                    controller: 'NavigationCtrl',
-                    templateUrl: 'views/master/navigation/navigation.view.html'
-                },
-                'breadcrumb@master': {
-                    controller: 'BreadcrumbCtrl',
-                    templateUrl: 'views/master/breadcrumb/breadcrumb.view.html'
-                }
+    $stateProvider.state('master', {
+        access: 'admin',
+        url: '/',
+        views: {
+            'header': {
+                controller: 'HeaderCtrl',
+                templateUrl: 'views/master/header/header.view.html'
+            },
+            'main': {
+                templateUrl: 'views/master/main.view.html'
+            },
+            'navigation@master': {
+                controller: 'NavigationCtrl',
+                templateUrl: 'views/master/navigation/navigation.view.html'
+            },
+            'breadcrumb@master': {
+                controller: 'BreadcrumbCtrl',
+                templateUrl: 'views/master/breadcrumb/breadcrumb.view.html'
             }
-        });
+        }
+    });
 })
 
 /**
@@ -46,7 +43,6 @@ angular.module('w3uiFrontendApp', [
   */
 .run(['$rootScope', '$state', 'Authentication',
     function ($rootScope,  $state, Authentication) {
-
         $rootScope.$on('$stateChangeStart', function(event, next) {
             //Check if state is open for public
             if( next.access !== 'public' ){
@@ -65,5 +61,4 @@ angular.module('w3uiFrontendApp', [
                 }
             }
         });
-
 }]);
